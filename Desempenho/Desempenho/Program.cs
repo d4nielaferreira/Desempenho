13abd6f21678b7200720b7188ee53a6c05c4cdb6
Lucasthemob <44687772+Lucasthemob@users.noreply.github.com>
2018-11-03 00:53:27 -0200

    Viadoes
    Churras 25/10

--- Desempenho/Desempenho/Program.cs
+++ Desempenho/Desempenho/Program.cs
@@ -10,47 +10,18 @@ namespace Desempenho
     {
         static void Main(string[] args)
         {
-            int achou, inicio, fim, meio, busca, TAM;
-            int[] vetor = new int[1000];
-            TAM = 1000;
-            achou = 0;
-            inicio = 0;
-            fim = TAM - 1; 
-            var populador = 0;
+            Binario Teste = new Binario();
+            Teste.PesquisaBinaria();
 
-            foreach (var posicao in vetor)
-            {
-                vetor[posicao] = populador + 1;
-            }
-
-            Console.WriteLine("Entre com um número inteiro a ser pesquisado: ");
-            busca = int.Parse(Console.ReadLine());
-
-            while (inicio <= fim)
-            {
-                meio = (inicio + fim) / 2;
-                Console.WriteLine("Meio= " + meio);
-                if (vetor[meio] == busca)
-                {
-                    achou = 1;
-                }
-                if (busca < vetor[meio])
-                {
-                    fim = meio - 1;
-                }
-                else
-                    inicio = meio + 1;
-            }
-
-            if (achou == 1)
-            {
-                Console.WriteLine("Valor ", +busca, " encontrado");
-            }
-            else
-                Console.WriteLine("Valor não encontrado");
-
-
+            int[] arr = { 10, 64, 7, 52, 32, 18, 2, 48 };
+            HeapSort hs = new HeapSort();
+            Console.WriteLine(hs.tempoinicio.ToString("h:mm:ss.fff tt"));
+            hs.PerformHeapSort(arr);
+            Console.WriteLine();
+            Console.WriteLine(hs.tempofinal.ToString("h:mm:ss.fff tt"));
             Console.ReadLine();
         }
+
+
     }
 }
